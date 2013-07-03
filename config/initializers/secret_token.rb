34e7195eac012bdf4ecb1f69f8fcc32069374236
Luke Robertson <luke.robertson@mac.com>
2013-07-03 18:13:14 +0100

    initializes structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamc::Application.config.secret_token = '46d3c8e18dc480b67c8fc1b61ab988b581212e83de3122f9a0eeae12169b243b77c312736800faecd3c60850ab703859bce46e28e0239071067042683c3122a2'
